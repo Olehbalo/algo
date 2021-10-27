98fa1e6cb7c60291e172a0da82c706d8f268d1db
Olehbalo <balo.oleh.p@gmail.com>
2021-10-27 20:10:31 +0300

    lab2

--- main.py
+++
@@ -1,7 +0,0 @@
-from labone import merge_sort
-import unittest
-
-
-mas= [int(i) for i in input().split(",")]
-n=str(input())
-print( merge_sort(mas,n))
--- test.py
+++ test.py
@@ -1,23 +1,19 @@
-from labone import merge_sort
 import unittest
 
+from HashTable import HashTable
 
-class TestMergeSort(unittest.TestCase):
-    def test_asc_sorting(self):
-        self.assertEqual(merge_sort([1, 3, -2, 0, 9], "asc"), [-2, 0, 1, 3, 9])
+class Test(unittest.TestCase):
 
-    def test_desc_sorting(self):
-        self.assertEqual(merge_sort([1, 3, -2, 0, 9], "desc"), [9, 3, 1, 0, -2])
+    def test_set_func(self):
+        hash_table = HashTable()
+        self.assertEqual(hash_table.set("Sony", 2800), [("Sony", 2800)])
 
-    def test_asc_sorting_sorted_by_asc(self):
-        self.assertEqual(merge_sort([1, 3, -2, 0, 9], "asc"), [-2, 0, 1, 3, 9])
+    def test_get_func(self):
+        hash_table = HashTable()
+        hash_table.set("Sony", 2800)
+        self.assertEqual(hash_table.get("Sony"), [("Sony", 2800)])
 
-    def test_asc_sorting_sorted_by_desc(self):
-        self.assertEqual(merge_sort([9, 3, 1, 0, -2], "asc"), [-2, 0, 1, 3, 9])
-
-    def test_desc_sorting_sorted_by_asc(self):
-        self.assertEqual(merge_sort([1, 3, -2, 0, 9], "desc"), [9, 3, 1, 0, -2])
-
-    def test_desc_sorting_sorted_by_desc(self):
-        self.assertEqual(merge_sort([9, 3, 1, 0, -2], "desc"), [9, 3, 1, 0, -2])
-
+    def test_delete_func(self):
+        hash_table = HashTable()
+        hash_table.set("Sony", 2800)
+        self.assertEqual(hash_table.delete("Sony"), "Sony") 
